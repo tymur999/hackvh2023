b2d3912c86b622344f0283539801ba1e3fed1447
tymur999 <tymur.arsent@gmail.com>
2023-01-05 21:14:52 -0600

    Navbar initialized

---
+++ src/comps/Navbar.tsx
@@ -0,0 +1,13 @@
+import "../scss/Navbar.scss";
+
+export function Navbar() {
+    return (
+        <>
+            <div className="d-flex nav">
+                <div className="nav-box ml-auto d-flex align-items-center">
+                    <h4 className="text-center fst-italic">HackVH</h4>
+                </div>
+            </div>
+        </>
+    )
+}
